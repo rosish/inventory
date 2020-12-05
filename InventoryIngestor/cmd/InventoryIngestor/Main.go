3a5c1746cc664052d2566c4dd428443e7936a3f0
rosish shakya <rosish_shakya@dell.com>
2020-12-05 02:31:45 -0600

    bug fix

--- InventoryIngestor/cmd/InventoryIngestor/Main.go
+++ InventoryIngestor/cmd/InventoryIngestor/Main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/rosish/inventory/InventoryIngestor/pkg/bmcClient"
+	"inventory/InventoryIngestor/pkg/bmcClient"
 )
 func main() {
 	fmt.Printf("\nRunning ingestor application\n")
-	fmt.Printf(bmcClient.FetchInventory())
+	fmt.Printf(bmcClient.FetchLiveInventory())
 }
 
 
