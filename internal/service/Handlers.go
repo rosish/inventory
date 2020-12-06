2e2310636e7c76900e456383c02b38c5a7376a29
rosish shakya <rosish_shakya@dell.com>
2020-12-06 16:34:29 -0600

    refactoring

--- internal/service/Handlers.go
+++ internal/service/Handlers.go
@@ -4,16 +4,16 @@ import (
 	"github.com/rosish/goModuleTest/pkg/logger"
 	"github.com/rosish/inventory/InventoryDb/pkg/bmcDbClient"
 	"github.com/rosish/inventory/InventoryIngestor/pkg/bmcClient"
+	"github.com/rosish/inventory/pkg/manager"
 	"net/http"
-
 )
 
 func InventoryAccessHandler(w http.ResponseWriter, r *http.Request) {
 	logger.Log()
-
+	manager.Discover()
 	data := bmcClient.FetchLiveInventory()
 	w.Write([]byte(data))
-
+	w.Write([]byte("\n"))
 	data1 := bmcDbClient.FetchCachedInventory()
 	w.Write([]byte(data1))
 }
